7fd4baf30c9ce9bcb95b48739c6506f1df44a321
psandeepaxioned <vikasb@axioned.com>
2022-06-28 18:38:31 +0530

    working on slider js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,26 @@
 /* Author: 
 
 */
+const contents = document.querySelectorAll('.slider')
+const listitems = document.querySelectorAll('nav ul li')
 
+listitems.forEach((item,idx) => {
+    item.addEventListener('click', () => {
+        hideallcontents()
+        hideallitems()
 
+        item.classList.add('active')
+        contents[idx].classList.add('show')
+    })
+})
+
+function hideallcontents(){
+    contents.forEach(slider => slider.classList.remove('show'))
+}
+
+function hideallitems(){
+    listitems.forEach(item => item.classList.remove('active'))
+}
 
 
 
